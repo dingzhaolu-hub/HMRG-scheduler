b19592ec94a772f3908dac92c56e989c1b87cf1f
dingzhaolu-hub <dingzhaolu@gmail.com>
2026-05-17 15:14:40 -0400

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  base: "/",
+  publicDir: false,
+  build: {
+    outDir: "dist",
+    emptyOutDir: true
+  },
+  plugins: [react()]
+});
